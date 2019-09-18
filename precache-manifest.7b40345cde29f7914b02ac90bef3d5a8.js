a8a0e9fcfe743b0e1e1778c0d3b618882abfc499
Robert Francisco McPherson <mcphersonr1@DIT-02SJ0WSG8WM.montclair.edu>
2019-09-18 00:23:34 -0400

    Updates

---
+++ precache-manifest.7b40345cde29f7914b02ac90bef3d5a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "091b5dadb588c44d41eaea93d6ffdafd",
+    "url": "/Landmark-Frontend/index.html"
+  },
+  {
+    "revision": "9567f94c5666c75fde62",
+    "url": "/Landmark-Frontend/static/css/main.802d1eac.chunk.css"
+  },
+  {
+    "revision": "bb7bb2445d575b9e91da",
+    "url": "/Landmark-Frontend/static/js/2.bd8deef4.chunk.js"
+  },
+  {
+    "revision": "9567f94c5666c75fde62",
+    "url": "/Landmark-Frontend/static/js/main.e847926e.chunk.js"
+  },
+  {
+    "revision": "b78dacf903343343309f",
+    "url": "/Landmark-Frontend/static/js/runtime~main.33812542.js"
+  }
+]);
