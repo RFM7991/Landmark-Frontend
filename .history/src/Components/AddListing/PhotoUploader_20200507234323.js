40a92880081ad06274637322c469e03971d0b5f5
Robert Francisco McPherson <mcphersonr1@DIT-02SJ0WSG8WM.montclair.edu>
2020-05-13 17:30:21 -0400

    update

---
+++ .history/src/Components/AddListing/PhotoUploader_20200507234323.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ImageUploader from 'react-images-upload';
+ 
+export default class PhotoUploader extends React.Component {
+ 
+    constructor(props) {
+        super(props);
+         this.state = { pictures: [] };
+         this.onDrop = this.onDrop.bind(this);
+    }
+ 
+    async onDrop(picture) {
+        let photosState = this.state.pictures
+        console.log("pic", picture, photosState)
+        let photos = photosState.concat(picture)
+       
+        await this.setState({ pictures: photos })
+        this.props.setPhotos(photos)
+
+        
+    }
+ 
+    render() {
+        return (
+            <ImageUploader
+                withIcon={true}
+                buttonText='Choose images'
+                onChange={this.onDrop}
+                imgExtension={['.jpg', '.gif', '.png', '.gif']}
+                maxFileSize={5242880}
+            />
+        );
+    }
+}
---
+++ .history/src/Components/AddListing/PhotoUploader_20200508000500.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ImageUploader from 'react-images-upload';
+ 
+export default class PhotoUploader extends React.Component {
+ 
+    constructor(props) {
+        super(props);
+         this.state = { pictures: [] };
+         this.onDrop = this.onDrop.bind(this);
+    }
+ 
+    onDrop(pictures) {
+  //      let pictures = this.state.pictures.concat(picture)
+
+        console.log("pic", picture, pictures)
+
+        this.setState({ pictures: pictures })
+        this.props.setPhotos(pictures)
+    }
+ 
+    render() {
+        return (
+            <ImageUploader
+                withIcon={true}
+                buttonText='Choose images'
+                onChange={this.onDrop}
+                imgExtension={['.jpg', '.gif', '.png', '.gif']}
+                maxFileSize={5242880}
+            />
+        );
+    }
+}
