cafd8cdabb378ce3657379175d85a650aa1b2779
Robert McPherson <robertmcpherson@Roberts-MacBook-Pro.local>
2020-12-21 20:23:04 -0500

    space

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import App from './Components/App';
+import * as serviceWorker from './Redux/serviceWorker';
+import { Provider } from 'react-redux'
+import store from './Redux/store'
+import { HashRouter as Router } from 'react-router-dom';
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(
+    <Provider store={store}>
+    <Router history basename={"https://rfm7991.github.io/Landmark-Frontend/"}>< App /></Router>
+    </Provider>,
+     rootElement
+)
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
